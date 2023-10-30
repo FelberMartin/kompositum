797a2c06b4ebb88bb2c1f809e8e7cad21a9f0cdc
Martin Felber <felber.martin3@gmail.com>
2023-10-30 20:34:14 +0100

    Added reset functionality to database

--- lib/data/database_initializer.dart
+++ lib/data/database_initializer.dart
@@ -4,28 +4,45 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 import 'compound.dart';
+import 'compound_origin.dart';
 
 class DatabaseInitializer {
-
   final CompoundOrigin compoundOrigin;
 
   DatabaseInitializer(this.compoundOrigin);
 
-  Future<Database> getInitializedDatabase() {
-    WidgetsFlutterBinding.ensureInitialized();
-    return getDatabasesPath().then((dbPath) {
-      final path = join(dbPath, "kompositum.db");
-      return openDatabase(
-        path,
-        version: 1,
-        onCreate: (db, version) async {
-          await createCompoundsTable(db);
-        },
-      );
-    });
+  Future<Database> getInitializedDatabase(
+      {bool reset = false, bool useInMemoryDatabase = false}) async {
+    String path = await _getPath(useInMemoryDatabase);
+    final db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await _createCompoundsTable(db);
+        await _insertCompoundsFromCompoundData(db);
+      },
+    );
+
+    if (reset) {
+      await db.delete("compounds");
+      await _insertCompoundsFromCompoundData(db);
+    }
+    return db;
   }
 
-  Future<void> createCompoundsTable(Database db) {
+  Future<String> _getPath(bool useInMemoryDatabase) async {
+    String path;
+    if (useInMemoryDatabase) {
+      path = inMemoryDatabasePath;
+    } else {
+      WidgetsFlutterBinding.ensureInitialized();
+      final dbPath = await getDatabasesPath();
+      path = join(dbPath, "kompositum.db");
+    }
+    return path;
+  }
+
+  Future<void> _createCompoundsTable(Database db) {
     return db.execute(
       "CREATE TABLE compounds ("
       "name TEXT PRIMARY KEY,"
@@ -36,16 +53,13 @@ class DatabaseInitializer {
     );
   }
 
-  Future<void> insertCompounds(Database db, List<Compound> compounds) {
-    // return rootBundle.loadString(csvFilePath).then((csv) {
-    //   final lines = csv.split("\n");
-    //   final compounds = lines.map((line) => Compound.fromCsvLine(line));
-    //   return db.transaction((txn) {
-    //     compounds.forEach((compound) {
-    //       txn.insert("compounds", compound.toMap());
-    //     });
-    //   });
-    // });
-    return Future.value();
+  Future<void> _insertCompoundsFromCompoundData(Database db) async {
+    await compoundOrigin.getCompounds().then((compoundData) {
+      final batch = db.batch();
+      for (final compound in compoundData) {
+        batch.insert("compounds", compound.toMap());
+      }
+      return batch.commit();
+    });
   }
 }
--- test/data/database_initializer_test.dart
+++ test/data/database_initializer_test.dart
@@ -1,10 +1,40 @@
 import 'package:kompositum/data/compound.dart';
+import 'package:kompositum/data/compound_origin.dart';
 import 'package:kompositum/data/database_initializer.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:test/test.dart';
 
+class MockCompoundOrigin extends Mock implements CompoundOrigin {}
+
 void main() async {
-  final sut = DatabaseInitializer();
+  late CompoundOrigin compoundOrigin;
+  late DatabaseInitializer sut;
+
+  final compoundsFromOrigin = [
+    const Compound(
+      name: "Krankenhaus",
+      modifier: "krank",
+      head: "Haus",
+      frequencyClass: 1,
+    ),
+  ];
+
+  // A different list of compounds to test if the database is initialized correctly
+  final compoundsFromOrigin2 = [
+    const Compound(
+      name: "Spielplatz",
+      modifier: "Spiel",
+      head: "Platz",
+      frequencyClass: 1,
+    ),
+    const Compound(
+      name: "Apfelbaum",
+      modifier: "Apfel",
+      head: "Baum",
+      frequencyClass: 1,
+    ),
+  ];
 
   // Setup sqflite_common_ffi for flutter test
   setUpAll(() {
@@ -14,13 +44,20 @@ void main() async {
     databaseFactory = databaseFactoryFfi;
   });
 
+  setUp(() {
+    compoundOrigin = MockCompoundOrigin();
+    when(() => compoundOrigin.getCompounds())
+        .thenAnswer((_) async => compoundsFromOrigin);
+    sut = DatabaseInitializer(compoundOrigin);
+  });
+
   /// For testing the ffi test framework
   test('Simple test', () async {
     var db = await openDatabase(inMemoryDatabasePath, version: 1,
         onCreate: (db, version) async {
-          await db
-              .execute('CREATE TABLE Test (id INTEGER PRIMARY KEY, value TEXT)');
-        });
+      await db
+          .execute('CREATE TABLE Test (id INTEGER PRIMARY KEY, value TEXT)');
+    });
     // Insert some data
     await db.insert('Test', {'value': 'my_value'});
     // Check content
@@ -33,49 +70,64 @@ void main() async {
 
   group("getInitializedDatabase", () {
     test(
-      "should return a database" ,
+      "should return a database",
       () async {
-        final database = await sut.getInitializedDatabase();
+        final database = await sut.getInitializedDatabase(useInMemoryDatabase: true);
         expect(database, isNotNull);
       },
     );
 
     test(
-      "should return a database with the compounds table" ,
-          () async {
-        final database = await sut.getInitializedDatabase();
-        final tables = await database.query("sqlite_master", where: "type = 'table'");
+      "should return a database with the compounds table",
+      () async {
+        final database = await sut.getInitializedDatabase(useInMemoryDatabase: true);
+        final tables =
+            await database.query("sqlite_master", where: "type = 'table'");
         expect(tables, isNotEmpty);
         expect(tables.first["name"], "compounds");
       },
     );
+
+    test(
+      "should return a database with the compounds from the origin",
+      () async {
+        final database = await sut.getInitializedDatabase(useInMemoryDatabase: true);
+
+        final compounds = await database.query("compounds");
+        expect(compounds, isNotEmpty);
+        expect(compounds, compoundsFromOrigin.map((e) => e.toMap()));
+      },
+    );
   });
 
-  group("insertCompounds", () {
-      test(
-        "should insert the given compounds into the database",
-        () async {
-          final database = await openDatabase(inMemoryDatabasePath);
-          final compounds = [
-            const Compound(
-              name: "Krankenhaus",
-              modifier: "krank",
-              head: "Haus",
-              frequencyClass: 1,
-            ),
-          ];
-          await sut.insertCompounds(database, compounds);
-          final result = await database.query("compounds");
-          expect(result, isNotEmpty);
-          expect(result.length, 1);
-          expect(result.first["name"], "Krankenhaus");
-          expect(result.first["modifier"], "krank");
-          expect(result.first["head"], "Haus");
-          expect(result.first["frequencyClass"], 1);
-        },
-      );
+  group("getInitializedDatabase with reset", () {
+    test(
+      "should leave the current compounds if reset is false ",
+      () async {
+        await sut.getInitializedDatabase(useInMemoryDatabase: true);
+        when(() => compoundOrigin.getCompounds())
+            .thenAnswer((_) async => compoundsFromOrigin2);
+        final database = await sut.getInitializedDatabase(useInMemoryDatabase: true, reset: false);
 
+        final compounds = await database.query("compounds");
+        expect(compounds, isNotEmpty);
+        expect(compounds, compoundsFromOrigin.map((e) => e.toMap()));
+      },
+    );
 
-    });
+    test(
+      "should reset the current compounds if reset is true ",
+      () async {
+        await sut.getInitializedDatabase(useInMemoryDatabase: true);
+        when(() => compoundOrigin.getCompounds())
+            .thenAnswer((_) async => compoundsFromOrigin2);
+        final database = await sut.getInitializedDatabase(useInMemoryDatabase: true, reset: true);
+
+        final compounds = await database.query("compounds");
+        expect(compounds, isNotEmpty);
+        expect(compounds, compoundsFromOrigin2.map((e) => e.toMap()));
+      },
+    );
+  });
 
 }
