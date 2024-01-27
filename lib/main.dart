893e0aa8f21b07e55ec4f0a4cf3721c70333ef28
Martin Felber <felber.martin3@gmail.com>
2024-01-27 13:17:13 +0100

    Adapted name to "Compose - Wörter kombinieren"

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
-      title: 'Compose',
+      title: 'Compose - Wörter kombinieren',
       theme: myTheme,
       locale: const Locale('de', 'DE'),
       home: HomePage(),
