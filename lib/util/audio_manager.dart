eff74409f3df3d065481bdeb55e7478e293b02f8
Martin Felber <felber.martin3@gmail.com>
2024-02-11 20:02:28 +0100

    Test sounds

--- lib/util/audio_manager.dart
+++ lib/util/audio_manager.dart
@@ -1,5 +1,8 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import '../config/my_theme.dart';
 
 class AudioManager {
 
@@ -31,11 +34,11 @@ class AudioManager {
   }
 
   void playStarCollected() {
-    _playAsset("score.mp3");
+    _playAsset("score.wav", volume: 0.4);
   }
 
   void playLevelComplete() {
-    _playAsset("success_02.wav", volume: 0.3);
+    _playAsset("success_02.wav");
   }
 
   void playHint() {
@@ -43,14 +46,14 @@ class AudioManager {
   }
 
   void playCompoundFound() {
-    _playAsset("correct.wav", volume: 0.15);
+    _playAsset("correct.wav", volume: 0.2);
   }
 
   void playCompoundIncorrect() {
-    _playAsset("incorrect.wav", volume: 0.3);
+    _playAsset("incorrect.wav");
   }
 
-  void _playAsset(String asset, {double volume = 0.4}) async {
+  void _playAsset(String asset, {double volume = 0.3}) async {
     if (isMuted) return;
     final player = AudioPlayer();
     player.setPlayerMode(PlayerMode.lowLatency);
@@ -59,4 +62,50 @@ class AudioManager {
     await player.resume();
   }
 
+}
+
+void main() {
+  runApp(MaterialApp(
+      theme: myTheme,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              AudioManager.instance.playCompoundFound();
+            },
+            child: Text("Compound found"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              AudioManager.instance.playCompoundIncorrect();
+            },
+            child: Text("Compound incorrect"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              AudioManager.instance.playStarCollected();
+            },
+            child: Text("Star collected"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              AudioManager.instance.playButtonClicked();
+            },
+            child: Text("Button clicked"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              AudioManager.instance.playLevelComplete();
+            },
+            child: Text("Level complete"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              AudioManager.instance.playHint();
+            },
+            child: Text("Hint"),
+          ),
+        ],
+      )));
 }
