e88d18a287f8c61614eb98ead924c36974584bb0
Martin Felber <felber.martin3@gmail.com>
2024-07-22 14:34:00 +0200

    Updated iOS share link

--- lib/util/my_share.dart
+++ lib/util/my_share.dart
@@ -4,7 +4,7 @@ class MyShare {
 
   static const String appName = 'Wort + Schatz: Wörter Suche';
   static const String playStoreUrl = 'https://play.google.com/store/apps/details?id=com.development_felber.compose';
-  static const String appStoreUrl = 'https://apps.apple.com/app/wort-schatz-w%C3%B6rter-suche/id6526461190';
+  static const String appStoreUrl = 'https://apps.apple.com/de/app/wort-schatz-w%C3%B6rter-suche/id6526461190';
 
   static Future<void> shareApp() {
     final shareText = "Entdecke jetzt meine neue Lieblings-App '$appName'!\n\n"
